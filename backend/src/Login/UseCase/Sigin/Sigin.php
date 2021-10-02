b84317b83b50068386bd7b2570230e706e352f44
Evelyn <evelynbrandao15@gmail.com>
2021-10-02 17:17:52 -0300

    Caso de Uso de Sigin

---
+++ backend/src/Login/UseCase/Sigin/Sigin.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TaskTime\Login\UseCase\Sigin;
+
+use Exception;
+use TaskTime\Login\UseCase\Repository\RepositoryInterface;
+
+class Sigin
+{
+	public RepositoryInterface $repository;
+	public TokenModel $token;
+
+	public function __construct(RepositoryInterface $repository, TokenModel $token)
+	{
+		$this->repository = $repository;
+		$this->token = $token;
+	}
+
+	public function execute(InputData $input)
+	{
+		$loginExist = $this->repository->getByEmail($input->email);
+
+		if(!$loginExist){
+			throw new Exception("Usuário não encontrado!");
+		}
+
+		// Verifica senhas
+		if(!password_verify($input->password, $loginExist->password)){
+			throw new Exception("Email ou senha inválidos!");
+		}
+
+		return OutputData::create([
+			"email" => $input->email,
+			"token" => $this->token->create([
+				"expiration_at" => "2h",
+			])
+		]);
+
+	}
+}
