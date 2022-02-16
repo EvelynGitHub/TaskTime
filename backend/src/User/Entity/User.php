19019cb39001c57e150e1ee61f1f2aa1db21e6e7
Evelyn F.B <evelynbrandao15@gmail.com>
2022-02-16 17:38:37 -0300

    Entidade de Usuario

---
+++ backend/src/User/Entity/User.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TaskTime\User\Entity;
+
+use TaskTime\Login\Entity\Login;
+
+class User
+{
+    public function __construct(
+        private string $uuid,
+        private string $firstName,
+        private string $lastName,
+        private Login $login
+    ) {
+    }
+
+    /**
+     * Get the value of uuid
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * Set the value of uuid
+     *
+     * @return  self
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of firstName
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @return  self
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @return  self
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of login
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set the value of login
+     *
+     * @return  self
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+}
