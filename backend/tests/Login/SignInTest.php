6b44e531ba3c4a9f0ebef26c54625207bb566081
Evelyn F.B <evelynbrandao15@gmail.com>
2022-06-01 18:14:32 -0300

    Ajuste em casos de teste ao logar

--- backend/tests/Login/SignInTest.php
+++ backend/tests/Login/SignInTest.php
@@ -12,8 +12,6 @@ use TaskTime\Login\Controller\SignInController;
 use TaskTime\Login\Entity\Exceptions\InvalidEmailValue;
 use TaskTime\Login\Entity\Login;
 use TaskTime\Login\Entity\ValueObject\Email;
-use TaskTime\Login\UseCase\SignIn\InputData;
-use TaskTime\Login\UseCase\SignIn\SignIn;
 use TaskTime\Login\UseCase\SignIn\TokenModel;
 
 use Tests\RequestTest as Request;
@@ -24,157 +22,157 @@ use Tests\ResponseTest as Response;
 
 class SignInTest extends TestCase
 {
-	/*
+    /*
 	 * Para testar essa classe de teste:
 	 * composer run test tests/Login/SignInTest.php
 	 * OU
 	 * php ./vendor/phpunit/phpunit/phpunit tests/Login/SignInTest.php --filter expectTokenIfLoginValid
 	 */
 
-	protected static $inicio = false;
-	protected static Request $request;
-	protected static Response $response;
-	protected static $controller;
-	protected static $loginRepository;
-	protected static Login $login;
+    protected static $inicio = false;
+    protected static Request $request;
+    protected static Response $response;
+    protected static $controller;
+    protected static $loginRepository;
+    protected static Login $login;
 
-	protected function setUp(): void
-	{
+    protected function setUp(): void
+    {
 
-		if (!self::$inicio) {
-			self::$request = new Request([
-				"email" => "admin@gmail.com",
-				"pass" => "123"
-			]);
-			self::$response = new Response();
-			self::$login = new Login('', new Email("admin@gmail.com"),'', '', '', '');
+        if (!self::$inicio) {
+            self::$request = new Request([
+                "email" => "admin@gmail.com",
+                "pass" => "123"
+            ]);
+            self::$response = new Response();
+            self::$login = new Login('', new Email("admin@gmail.com"), '', '', '', '');
 
-			$adapter = new JWTAdapter();
-			$token = new TokenModel($adapter);			
+            $adapter = new JWTAdapter();
+            $token = new TokenModel($adapter);
 
-			self::$loginRepository = $this->createMock(LoginRepository::class);
-			self::$controller = new SignInController(self::$loginRepository, $token);
-			
-			self::$inicio = true;
-		}
-	}
+            self::$loginRepository = $this->createMock(LoginRepository::class);
+            self::$controller = new SignInController(self::$loginRepository, $token);
 
-	/**
-	 * Deveria retornar erro caso o email não seja encontrado
-	 * @test
-	 */
-	public function expectExceptionUserNotFound()
-	{
-		// ======== Arrumar ========		
-		self::$loginRepository->method('getByEmail')->willReturn(null);
+            self::$inicio = true;
+        }
+    }
 
-		// ======== Afirmar ========
-		$this->expectException(Exception::class);
-		$this->expectExceptionMessage("Usuário não encontrado!");
+    /**
+     * Deveria retornar erro caso o email não seja encontrado
+     * @test
+     */
+    public function expectExceptionUserNotFound()
+    {
+        // ======== Arrumar ========		
+        self::$loginRepository->method('getByEmail')->willReturn(null);
 
-		// ======== Agir ========
-		self::$controller->handler(self::$request, self::$response);
-	}
+        // ======== Afirmar ========
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Usuário não encontrado!");
 
-	/**
-	 * Deveria retornar erro se email não for informado
-	 * @test
-	 */
-	public function expectExceptionIfEmailNotFound()
-	{
-		// ======== Arrumar ========		
-		self::$request->setNewValue("email", null);
+        // ======== Agir ========
+        self::$controller->handler(self::$request, self::$response);
+    }
 
-		// ======== Afirmar ========
-		$this->expectException(InvalidEmailValue::class);
-		$this->expectExceptionMessage("Obrigatório informar o email!");
+    /**
+     * Deveria retornar erro se email não for informado
+     * @test
+     */
+    public function expectExceptionIfEmailNotFound()
+    {
+        // ======== Arrumar ========		
+        self::$request->setNewValue("email", null);
 
-		// ======== Agir ========
-		self::$controller->handler(self::$request, self::$response);
-	}
+        // ======== Afirmar ========
+        $this->expectException(InvalidEmailValue::class);
+        $this->expectExceptionMessage("Obrigatório informar o email!");
 
-	/**
-	 * Deveria retornar erro se email não for informado
-	 * @test
-	 */
-	public function expectExceptionIfEmailInvalid()
-	{
-		// ======== Arrumar ========		
-		self::$request->setNewValue("email", "test123");
+        // ======== Agir ========
+        self::$controller->handler(self::$request, self::$response);
+    }
 
-		// ======== Afirmar ========
-		$this->expectException(InvalidEmailValue::class);
-		$this->expectExceptionMessage("Email Inválido");
+    /**
+     * Deveria retornar erro se email não for informado
+     * @test
+     */
+    public function expectExceptionIfEmailInvalid()
+    {
+        // ======== Arrumar ========		
+        self::$request->setNewValue("email", "test123");
 
-		// ======== Agir ========
-		self::$controller->handler(self::$request, self::$response);
-	}
+        // ======== Afirmar ========
+        $this->expectException(InvalidEmailValue::class);
+        $this->expectExceptionMessage("Email Inválido");
 
-	/**
-	 * Deveria retornar erro se não informar a senha
-	 * @test
-	 */
-	public function expectExceptionIfPasswordNotFound()
-	{
-		// ======== Arrumar ========		
-		self::$request->setNewValue("email", "teste@test.com");
-		self::$request->setNewValue("pass", null);
+        // ======== Agir ========
+        self::$controller->handler(self::$request, self::$response);
+    }
 
-		// ======== Afirmar ========
-		$this->expectException(Exception::class);
-		$this->expectExceptionMessage("Obrigatório informar a senha!");
+    /**
+     * Deveria retornar erro se não informar a senha
+     * @test
+     */
+    public function expectExceptionIfPasswordNotFound()
+    {
+        // ======== Arrumar ========		
+        self::$request->setNewValue("email", "teste@test.com");
+        self::$request->setNewValue("pass", null);
 
-		// ======== Agir ========
-		self::$controller->handler(self::$request, self::$response);
-	}
+        // ======== Afirmar ========
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Obrigatório informar a senha!");
 
-	/**
-	 * Deveria retornar erro se não informar a senha
-	 * @test
-	 */
-	public function expectExceptionIfPasswordInvalid()
-	{
-		// ======== Arrumar ========
-		$email = new Email("email_teste@gmail.com");
+        // ======== Agir ========
+        self::$controller->handler(self::$request, self::$response);
+    }
 
-		self::$login->setEmail($email);
-		self::$login->setPassword("123");
+    /**
+     * Deveria retornar erro se não informar a senha correta
+     * @test
+     */
+    public function expectExceptionIfPasswordInvalid()
+    {
+        // ======== Arrumar ========
+        $email = new Email("email_teste@gmail.com");
 
-		self::$request->setNewValue("email", $email);
-		self::$request->setNewValue("pass", '12');
+        self::$login->setEmail($email);
+        self::$login->setPassword("123");
 
-		self::$loginRepository->method('getByEmail')->willReturn(self::$login);
+        self::$request->setNewValue("email", $email);
+        self::$request->setNewValue("pass", '12');
 
-		// ======== Afirmar ========
-		$this->expectException(Exception::class);
-		$this->expectExceptionMessage("Email ou senha incorreto!");
+        self::$loginRepository->method('getByEmail')->willReturn(self::$login);
 
-		// ======== Agir ========
-		self::$controller->handler(self::$request, self::$response);
-	}
+        // ======== Afirmar ========
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Email ou senha incorreto!");
 
-	/**
-	 * Deveria retornar Token se email e senha forem corretos
-	 * @test
-	 */
-	public function expectTokenIfLoginValid()
-	{
-		// ======== Arrumar ========
-		$email = new Email("email_teste@gmail.com");
-		$pass = password_hash("abc123", PASSWORD_DEFAULT);
+        // ======== Agir ========
+        self::$controller->handler(self::$request, self::$response);
+    }
 
-		self::$login->setEmail($email);
-		self::$login->setPassword($pass);
+    /**
+     * Deveria retornar Token se email e senha forem corretos
+     * @test
+     */
+    public function expectTokenIfLoginValid()
+    {
+        // ======== Arrumar ========
+        $email = new Email("email_teste@gmail.com");
+        $pass = password_hash("abc123", PASSWORD_DEFAULT);
 
-		self::$request->setNewValue("email", $email);
-		self::$request->setNewValue("pass", "abc123");
+        self::$login->setEmail($email);
+        self::$login->setPassword($pass);
 
-		self::$loginRepository->method('getByEmail')->willReturn(self::$login);
+        self::$request->setNewValue("email", $email);
+        self::$request->setNewValue("pass", "abc123");
 
-		// ======== Agir ========
-		self::$controller->handler(self::$request, self::$response);
+        self::$loginRepository->method('getByEmail')->willReturn(self::$login);
 
-		// ======== Afirmar ========
-		$this->assertNotEmpty(self::$response->getReturn());
-	}
+        // ======== Agir ========
+        self::$controller->handler(self::$request, self::$response);
+
+        // ======== Afirmar ========
+        $this->assertNotEmpty(self::$response->getReturn());
+    }
 }
